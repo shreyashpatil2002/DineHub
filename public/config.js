d6ec85a24887ed70134c328dbb1322b4c05fccac
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-01-11 07:57:55 +0000

    pages are creating / work in progress

---
+++ public/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9ZAKxJ701Siixxpeu_qMyaeOCEwlANlk",
+  authDomain: "resto-53821.firebaseapp.com",
+  databaseURL:
+    "https://resto-53821-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "resto-53821",
+  storageBucket: "resto-53821.appspot.com",
+  messagingSenderId: "326918236717",
+  appId: "1:326918236717:web:915e33ed359e463465e6a4",
+};
+
+export const app = initializeApp(firebaseConfig);
