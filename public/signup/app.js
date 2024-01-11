b42d0d16efaeec97c9852468128fe7e396b8c879
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-01-11 07:55:50 +0000

    signup page is created

---
+++ public/signup/app.js
@@ -0,0 +1,114 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  OAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { app } from "../config.js";
+
+const auth = getAuth(app);
+
+// creating a new user with their email and password
+let formSubmitBtn = document.getElementById("signupBtn");
+let inputFields = document.querySelectorAll(".form form input");
+let errBox = document.getElementById("errMsg");
+let errors = [];
+formSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputFields[0].value != "" && inputFields[0].value != null) {
+    if (inputFields[1].value != "" && inputFields[1].value != null) {
+      if (inputFields[2].value != "" && inputFields[2].value != null) {
+        if(inputFields[2].value.length >= 6) {
+          if (inputFields[3].checked == true) {
+            let email = inputFields[1].value;
+            let password = inputFields[2].value;
+            createUserWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                console.log(userCredential.user);
+              })
+              .catch((error) => {
+                console.log(error.code);
+                console.log(error.message);
+              });
+          } else {
+            errBox.innerText = `Accept the terms and policy 🚨`;
+            setInterval(() => {
+              errBox.style.animation =
+                "error 3s cubic-bezier(0.18, 0.87, 0.63, 1.20)";
+            }, 500);
+            errBox.removeAttribute("style");
+          }
+        }
+        else {
+          errBox.innerText = `Password must be atleast 6 characters. 😬`;
+          setInterval(() => {
+            errBox.style.animation =
+              "error 3s cubic-bezier(0.18, 0.87, 0.63, 1.20)";
+          }, 500);
+          errBox.removeAttribute("style");
+        }
+      } else {
+        errBox.innerText = `Password is required. 😬`;
+        setInterval(() => {
+          errBox.style.animation =
+            "error 3s cubic-bezier(0.18, 0.87, 0.63, 1.20)";
+        }, 500);
+        errBox.removeAttribute("style");
+      }
+    } else {
+      errBox.innerText = "Email is required. 🙄";
+      setInterval(() => {
+        errBox.style.animation =
+          "error 3s cubic-bezier(0.18, 0.87, 0.63, 1.20)";
+      }, 500);
+      errBox.removeAttribute("style");
+    }
+  } else {
+    errBox.innerText = "Name is required. 😒";
+    setInterval(() => {
+      errBox.style.animation = "error 3s cubic-bezier(0.18, 0.87, 0.63, 1.20)";
+    }, 500);
+    errBox.removeAttribute("style");
+  }
+});
+
+// signin with google Oauth provider
+let googleSigninBtn = document.getElementById('googleSigninBtn');
+googleSigninBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log(credential, token, user)
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorCode, errorMessage, email, credential)
+  });
+})
+
+let appleSigninBtn = document.getElementById('appleSigninBtn');
+appleSigninBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const provider = new OAuthProvider('apple.com');
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log(credential, token, user)
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorCode, errorMessage, email, credential)
+  });
+})
+
