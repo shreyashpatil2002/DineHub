04d5c355aed2c10bde32dedc841bf7e4461679f6
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-01-27 04:04:53 +0000

    completed the UI and UX part of home page

---
+++ public/main.js
@@ -0,0 +1,117 @@
+let tl = gsap.timeline();
+tl.from("header", {
+  duration: 1,
+  y: "-100%",
+  opacity: 0,
+});
+tl.from("#pageOne .container .content", {
+  duration: 1,
+  y: "50",
+  opacity: 0,
+});
+tl.from("#pageOne .container .img", {
+  duration: 1,
+  scale: 0.5,
+  opacity: 0,
+  ease: "bounce",
+});
+gsap.from(
+  "#pageTwo .container .content span, #pageTwo .container .content img, #pageTwo .container .content h2, #pageTwo .container .content p, #pageTwo .container .content button",
+  {
+    scrollTrigger: {
+      trigger: "#pageTwo",
+      scroller: "#hero",
+      start: "20% center",
+    },
+    duration: 1,
+    x: "-150",
+    opacity: 0,
+    stagger: 0.5,
+  }
+);
+gsap.from(
+  "#pageTwo .container .img img",
+  {
+    scrollTrigger: {
+      trigger: "#pageTwo",
+      scroller: "#hero",
+      start: "20% center",
+    },
+    duration: 1,
+    y: "150",
+    opacity: 0,
+    stagger: 0.5,
+  }
+);
+gsap.from(
+  "#pageThree .container .content span, #pageThree .container .content img, #pageThree .container .content h2, #pageThree .container .content p, #pageThree .container .content button",
+  {
+    scrollTrigger: {
+      trigger: "#pageThree",
+      scroller: "#hero",
+      start: "20% center",
+    },
+    duration: 1,
+    y: "-150",
+    opacity: 0,
+    stagger: 0.5,
+  }
+);
+gsap.from(
+  "#pageThree .container .img img",
+  {
+    scrollTrigger: {
+      trigger: "#pageThree",
+      scroller: "#hero",
+      start: "20% center",
+    },
+    duration: 1,
+    y: "150",
+    opacity: 0,
+    stagger: 0.5,
+  }
+);
+gsap.from(
+  "#pageFour .container .content span, #pageFour .container .content img, #pageFour .container .content h2, #pageFour .container .content p",
+  {
+    scrollTrigger: {
+      trigger: "#pageFour",
+      scroller: "#hero",
+      start: "20% center",
+    },
+    duration: 1,
+    y: "-150",
+    opacity: 0,
+    stagger: 0.5,
+  }
+);
+gsap.from("#pageFour .container .testimonials .testimonial", {
+  scrollTrigger: {
+    trigger: "#pageFour",
+    scroller: "#hero",
+    start: "20% center",
+  },
+  x: "150",
+  opacity: 0,
+  stagger: 0.5,
+}); 
+gsap.from("#pageFive .cards .card", {
+  scrollTrigger: {
+    trigger: "#pageFive",
+    scroller: "#hero",
+    start: "20% center",
+  },
+  y: "-150",
+  opacity: 0,
+  stagger: 0.8,
+}); 
+gsap.from("#pageSix .container", {
+  scrollTrigger: {
+    trigger: "#pageSix",
+    scroller: "#hero",
+    start: "center center",
+    scrub: 2,
+    pin: true,
+  },
+  scale: 2,
+}); 
