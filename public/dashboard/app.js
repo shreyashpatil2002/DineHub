3057fd21290e814639ce2d9e99844adc527b7b6a
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-02-06 10:08:54 +0000

    preloader and the javascript code is added

--- public/dashboard/app.js
+++ public/dashboard/app.js
@@ -1,10 +1,167 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
-  getFirestore,
-  collection,
-  setDoc,
-  doc,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { app } from "../config.js";
 
 const auth = getAuth(app);
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "../login";
+  }
+  else {
+    document.querySelector(".preloader").style.display = "none";
+  }
+});
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const user = localStorage.getItem("user");
+const userObj = JSON.parse(user);
+
+const db = getFirestore(app);
+const itemsCollection = collection(db, `restaurant/${userObj.uid}/foodItem`);
+console.log(userObj.photoURL);
+document.getElementById("profile").innerHTML = `
+              <img src="${userObj.photoURL === undefined ? '../Assets/svg/user-solid.svg' : userObj.photoURL}" alt="user image">
+              <p>${userObj.displayName} <img src="../Assets/svg/chevron-down-solid.svg" alt="dropDown"></p>`;
+
+// Update the list of items when any change happens in the Firestore
+onSnapshot(itemsCollection, (snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    const docData = change.doc.data();
+    const docId = change.doc.id;
+    if (docData.itemName !== undefined && change.type === "added") {
+      document.getElementById("foodItems").innerHTML += `
+        <div class="foodItem" id="${docId}">
+        <div class="itemImg" style="background-image: url('${docData.itemImg}')"></div>
+        <h2>${docData.itemName}</h2>
+        <p>Price: ${docData.itemPrice} Rs</p>
+        <p>Description: ${docData.itemDesc}</p>
+        <button class="editButton"><span class="material-symbols-outlined">
+          edit_square
+        </span> Edit</button>
+        <button class="deleteButton"><span class="material-symbols-outlined">
+        delete
+        </span> Delete</button>
+        <div class="editForm">
+          <button class="closeButton"><span class="material-symbols-outlined">close</span></button>
+          <label>Category</label>
+          <select class="itemCategory">
+            <option value="veg">Veg</option>
+            <option value="non-veg">Non-Veg</option>
+            <option value="Fast Food">Fast Food</option>
+            <option value="Hot Drinks">Hot Drinks</option>
+            <option value="Cold Drinks">Cold Drinks</option>
+          </select>
+          <label>Edit Name</label>
+          <input type="text" class="itemName" value="${docData.itemName}" />
+          <label>Edit Price</label>
+          <input type="number" class="itemPrice" value="${docData.itemPrice}" />
+          <label>Edit Description</label>
+          <input type="text" class="itemDesc" value="${docData.itemDesc}" />
+          <label>Available</label>
+          <select class="itemAvailable">
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          <button class="updateButton">Update</button>
+        </div>
+      </div>`;
+    }
+    if (change.type === "removed") {
+      document.getElementById(docId).remove();
+    }
+    if (change.type === "modified") {
+      const item = document.getElementById(docId);
+      item.querySelector("h2").innerText = docData.itemName;
+      item.querySelector("p").innerText = `Price: ${docData.itemPrice} Rs`;
+      item.querySelector(
+        "p:nth-of-type(2)"
+      ).innerText = `Description: ${docData.itemDesc}`;
+    }
+    enableOperations();
+  });
+});
+
+function enableOperations() {
+  document.querySelectorAll(".deleteButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (!confirm("Are you sure you want to delete this item?")) return;
+      const docId = e.target.parentElement.id;
+      deleteDoc(doc(db, `restaurant/${userObj.uid}/foodItem/${docId}`))
+        .then(() => {
+          console.log("Document successfully deleted!");
+        })
+        .catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+    });
+  });
+  document.querySelectorAll(".editButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      document.querySelector(".overlay").style.display = "block";
+      document.querySelector(".editForm").style.display = "block";
+    });
+  });
+  document.querySelectorAll(".closeButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      document.querySelector(".overlay").style.display = "none";
+      document.querySelector(".editForm").style.display = "none";
+    });
+  });
+  document.querySelectorAll(".updateButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      document.querySelector(".overlay").style.display = "none";
+      const docId = e.target.parentElement.parentElement.id;
+      const category =
+        e.target.parentElement.querySelector(".itemCategory").value;
+      const itemName = e.target.parentElement.querySelector(".itemName").value;
+      const itemPrice =
+        e.target.parentElement.querySelector(".itemPrice").value;
+      const itemDesc = e.target.parentElement.querySelector(".itemDesc").value;
+      const itemAvailable =
+        e.target.parentElement.querySelector(".itemAvailable").value;
+      updateDoc(doc(db, `restaurant/${userObj.uid}/foodItem/${docId}`), {
+        itemCategory: category,
+        itemName: itemName,
+        itemPrice: itemPrice,
+        itemDesc: itemDesc,
+        available: itemAvailable,
+      })
+        .then(() => {
+          document.querySelector(".editForm").style.display = "none";
+        })
+        .catch((error) => {
+          console.error("Error updating document: ", error);
+        });
+    });
+  });
+}
+document.getElementById("dropdown").innerHTML = `
+  <span id="profileClose"class="material-symbols-outlined">close</span>
+  <p><img src="${userObj.photoURL === undefined ? '../Assets/svg/user-solid.svg' : userObj.photoURL}" alt="profile pic"></p>
+  <p>${userObj.displayName}</p>
+  <p>${userObj.email}</p>
+  <button id="logout"><span class="material-symbols-outlined">
+  logout
+  </span>Logout</button>`;
+
+document.getElementById("profile").addEventListener("click", () => {
+  document.getElementById("dropdown").style.display = "block";
+  document.getElementById("profileClose").addEventListener("click", () => {
+    document.getElementById("dropdown").style.display = "none";
+  });
+  document.getElementById("logout").addEventListener("click", () => {
+    signOut(auth);
+    localStorage.removeItem("user");
+    window.location.href = "../login";
+  });
+});
--- public/dashboard/settings/app.js
+++ public/dashboard/settings/app.js
@@ -1,4 +1,4 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   getFirestore,
   collection,
@@ -8,35 +8,94 @@ import {
 import { app } from "../../config.js";
 
 const auth = getAuth(app);
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "../../login";
+  }
+  else {
+    document.querySelector(".preloader").style.display = "none";
+  }
+});
 
-var options = {
-  series: [{
-  name: 'sales',
-  data: [31, 40, 28, 51, 42, 109, 100]
-}],
-  chart: {
-  toolbar: {
-    show: false
-  },
-  height: 350,
-  type: 'area'
-},
-dataLabels: {
-  enabled: false
-},
-stroke: {
-  curve: 'smooth'
-},
-xaxis: {
-  type: 'datetime',
-  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-},
-tooltip: {
-  x: {
-    format: 'dd/MM/yy dd'
-  },
-},
-};
+const user = localStorage.getItem("user");
+const userObj = JSON.parse(user);
 
-var chart = new ApexCharts(document.querySelector("#incomeChart"), options);
-chart.render();
+document.getElementById("profile").innerHTML = `
+              <img src="${userObj.photoURL}" alt="user image">
+              <p>${userObj.displayName} <img src="../../Assets/svg/chevron-down-solid.svg" alt="dropDown"></p>`;
+
+function enableOperations() {
+  document.querySelectorAll(".deleteButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const docId = e.target.parentElement.id;
+      deleteDoc(doc(db, `restaurant/${userObj.uid}/foodItem/${docId}`))
+        .then(() => {
+          console.log("Document successfully deleted!");
+        })
+        .catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+    });
+  });
+  document.querySelectorAll(".editButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      document.querySelector(".editForm").style.display = "block";
+    });
+  });
+  document.querySelectorAll(".closeButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      document.querySelector(".editForm").style.display = "none";
+    });
+  });
+  document.querySelectorAll(".updateButton").forEach((button) => {
+    button.addEventListener("click", (e) => {});
+    document.querySelectorAll(".updateButton").forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const docId = e.target.parentElement.parentElement.id;
+        const category =
+          e.target.parentElement.querySelector(".itemCategory").value;
+        const itemName =
+          e.target.parentElement.querySelector(".itemName").value;
+        const itemPrice =
+          e.target.parentElement.querySelector(".itemPrice").value;
+        const itemDesc =
+          e.target.parentElement.querySelector(".itemDesc").value;
+        const itemAvailable =
+          e.target.parentElement.querySelector(".itemAvailable").value;
+        updateDoc(doc(db, `restaurant/${userObj.uid}/foodItem/${docId}`), {
+          itemCategory: category,
+          itemName: itemName,
+          itemPrice: itemPrice,
+          itemDesc: itemDesc,
+          available: itemAvailable,
+        })
+          .then(() => {
+            document.querySelector(".editForm").style.display = "none";
+          })
+          .catch((error) => {
+            console.error("Error updating document: ", error);
+          });
+      });
+    });
+  });
+}
+document.getElementById('dropdown').innerHTML = `
+  <span id="profileClose"class="material-symbols-outlined">close</span>
+  <p><img src="${userObj.photoURL}" alt="profile pic"></p>
+  <p>${userObj.displayName}</p>
+  <p>${userObj.email}</p>
+  <button id="logout"><span class="material-symbols-outlined">
+  logout
+  </span>Logout</button>`;
+
+document.getElementById("profile").addEventListener("click", () => { 
+  document.getElementById("dropdown").style.display = "block";
+  document.getElementById('profileClose').addEventListener('click', () => {
+    document.getElementById('dropdown').style.display = 'none';
+  });
+  document.getElementById("logout").addEventListener("click", () => {
+    signOut(auth);
+    localStorage.removeItem("user");
+    window.location.href = "../login";
+  });
+});
