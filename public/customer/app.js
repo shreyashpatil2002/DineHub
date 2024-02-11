63f84bed1227ee80b75004e5336858166934848f
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-02-11 07:19:19 +0000

    Refactor app.js and index.html

--- public/customer/app.js
+++ public/customer/app.js
@@ -1,43 +1,54 @@
-import { app } from '../config.js';
-import { getFirestore,
-    getDoc,
-    doc
-} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { app } from "../config.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let data = document.URL.split("=");
 
-// const DineCust = {
-//     tableNo : data[2],
-//     restId : data[1].split('@')[0]
-// }
+const DineCust = {
+  tableNo: data[2],
+  restId: data[1].split("@")[0],
+};
+localStorage.setItem("DineCust", JSON.stringify(DineCust));
 
-// localStorage.setItem('DineCust', JSON.stringify(DineCust))
+const restId = data[1].split("@")[0];
 
-document.getElementById('continue').addEventListener('click', () => {
-    const restCode = document.querySelector('input').value;
-    if(restCode === '') {
-        document.querySelector('.overlay').style.display = 'flex';
-        return;
+let RestData;
+// getting restuarant data from firestore
+const db = getFirestore(app);
+const docRef = doc(db, "restaurant", restId);
+getDoc(docRef)
+  .then((doc) => {
+    if (doc.exists()) {
+      RestData = doc.data();
+      // giving the restname to the header
+      document.querySelector(".restName").textContent = RestData.RestaurantName;
+    } else {
+      console.log("No such document!");
     }
-    const db = getFirestore(app);
-    const restRef = doc(db, 'restaurants', restCode);
-    getDoc(restRef).then((doc) => {
-        if (doc.exists()) {
-            const restData = doc.data();
-            if (restData.id === DineCust.restId) {
-                localStorage.setItem('DineCust', JSON.stringify(DineCust));
-                window.location.href = 'menu.html';
-            } else {
-                alert('Invalid Restaurant Code');
-            }
-        } else {
-            alert('Invalid Restaurant Code');
-        }
-    }).catch((error) => {
-        console.log('Error getting document:', error);
-    });
+  })
+  .catch((error) => {
+    console.log("Error getting document:", error);
+  });
+
+document.getElementById("continue").addEventListener("click", () => {
+  const restCode = document.querySelector("input").value;
+  if (restCode === "") {
+    document.querySelector(".overlay").style.display = "flex";
+    return;
+  }
+  validateCode(restCode);
+});
+document.querySelector(".closeWarning").addEventListener("click", () => {
+  document.querySelector(".overlay").style.display = "none";
 });
 
-document.querySelector('.closeWarning').addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = 'none';
-});
+const validateCode = (restCode) => {
+  if (RestData.RestCode === restCode) {
+    window.location.href = `./menus`;
+  } else {
+    document.querySelector(".overlay").style.display = "flex";
+  }
+};
