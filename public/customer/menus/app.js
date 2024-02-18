88766c394ccc88af72696acb846af1c9d80f4c6b
Shreyash Patil <108810152+shreyashpatil2002@users.noreply.github.com>
2024-02-18 08:49:15 +0000

    Refactor menu display and add cart icon

--- public/customer/menus/app.js
+++ public/customer/menus/app.js
@@ -17,9 +17,6 @@ const collectionRef = collection(db, "restaurant", restId, "foodItem");
 onSnapshot(collectionRef, (snapshot) => {
   snapshot.docChanges().forEach((change) => {
     if (change.type === "added") {
-      if (change.doc.data().available === "false") {
-        return;
-      }
       const foodItem = change.doc.data();
       const menuElement = document.getElementById("menu");
       const category = foodItem.itemCategory;
@@ -32,7 +29,9 @@ onSnapshot(collectionRef, (snapshot) => {
         menuElement.appendChild(categoryHeading);
       }
 
-      let itemList = document.querySelector(`#${categoryHeadingId} + .itemList`);
+      let itemList = document.querySelector(
+        `#${categoryHeadingId} + .itemList`
+      );
       if (!itemList) {
         itemList = document.createElement("div");
         itemList.classList.add("itemList");
@@ -60,18 +59,43 @@ onSnapshot(collectionRef, (snapshot) => {
         <h3>${foodItem.itemName}</h3>
         <p>Description: ${description}</p>
         <p>Price: $${foodItem.itemPrice}</p>
-        <button>Add to Cart</button>
+        <button onclick="addItem(this.parentElement)">Add to Cart</button>
       `;
 
       itemList.appendChild(foodItemElement);
+      if (change.doc.data().available === "false") {
+        document.getElementById(change.doc.id).style.display = "none";
+      }
     }
     if (change.type === "modified") {
+      // console.log(change.doc.id);
       if (change.doc.data().available === "false") {
         document.getElementById(change.doc.id).style.display = "none";
       }
       if (change.doc.data().available === "true") {
         document.getElementById(change.doc.id).style.display = "block";
       }
+
+      const MAX_DESCRIPTION_LENGTH = 5;
+      const truncatedDescription = change.doc
+        .data()
+        .itemDesc.split(" ")
+        .slice(0, MAX_DESCRIPTION_LENGTH)
+        .join(" ");
+
+      // Add dots if the description was truncated
+      const description =
+        change.doc.data().itemDesc.length > MAX_DESCRIPTION_LENGTH
+          ? truncatedDescription + "..."
+          : truncatedDescription;
+
+      document.getElementById(change.doc.id).innerHTML = `
+        <img src="${change.doc.data().itemImg}" alt="${change.doc.data().itemName}" />
+        <h3>${change.doc.data().itemName}</h3>
+        <p>Description: ${description}</p>
+        <p>Price: $${change.doc.data().itemPrice}</p>
+        <button onclick="addItem(this.parentElement)>Add to Cart</button>
+      `;
     }
     if (change.type === "removed") {
       document.getElementById(change.doc.id).remove();
